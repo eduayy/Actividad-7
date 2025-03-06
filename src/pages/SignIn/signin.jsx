c628955faba316d8d723adb279db0edbc00a1ad4
eduayy <cesr031223@gmail.com>
2025-03-05 18:50:12 -0600

    Log In frontend funcionality implemented

--- src/pages/SignIn/signin.jsx
+++ src/pages/SignIn/signin.jsx
@@ -1,7 +1,33 @@
 import "./signin.css";
 
 function SignIn() {
-  return <></>;
+  return (
+    <div className="Window">
+      <div className="Header">
+        <h1>Investigadores</h1>
+      </div>
+      <div className="LogIn">
+        <form className="Form">
+          <h1>Iniciar Sesión</h1>
+          <input
+            className="input"
+            type="text"
+            placeholder="Usuario o Correo"
+            autoComplete="off"
+            autoCapitalize="off"
+          />
+          <input
+            className="inputs"
+            type="password"
+            placeholder="Contraseña"
+            autoComplete="off"
+            autoCapitalize="off"
+          />
+          <button type="submit">Iniciar Sesión</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
